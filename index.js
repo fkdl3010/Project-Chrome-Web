d578f7098ddd0c10c4e2752d73eb2740291cdb9a
fkdl3010 <fkdl3919@icloud.com>
2021-01-13 02:30:26 +0900

    update

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-console.log('Im Working.');
-var name = 'bathingape'
-console.log(name); // bathingape
+const title = document.getElementById("title");
 
-(()=>{
-    
-    var name = 'javascript';
-    console.log(name);
-}) ();
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = 'rgb(52, 152, 219)';
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if(currentColor === BASE_COLOR){
+    title.style.color = OTHER_COLOR;
+  }else{
+    title.style.color = BASE_COLOR;
+  }
+  
+}
+function init() {
+    title.addEventListener("mouseenter", handleClick);
+    title.style.color = BASE_COLOR;
+  }
+init();
+
+function handelOffline(){
+  console.log('hihihihihi');
+}
+window.addEventListener("offline",handelOffline);
+window.addEventListener("online",handelOffline);
