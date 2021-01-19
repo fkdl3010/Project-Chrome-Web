7c256286a5024c3f0a6d9f0c34dad5cd52cfb217
fkdl3010 <fkdl3919@icloud.com>
2021-01-20 08:25:32 +0900

    [add comment 2021-01-20]

--- todo.js
+++ todo.js
@@ -6,10 +6,13 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
+// localStorage에 toDos 키로 value를 toDos의 배열로 넣어줌
+// localStorage는 String형태만 저장하기때문에 객체 또는 배열을 문자열로 변환하는JSON.stringify 사용
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+// text를 li에 담아 ul 밑에 뿌려줌, toDos 배열에 담아줌
 function paintToDo(text){
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
@@ -36,6 +39,7 @@ function handleSubmit(event){
     toDoInput.value = '';
 }
 
+// localStorage에 있는 toDo를 가져와 객체로 만든 뒤 화면에 뿌림
 function loadToDos(){
     const loadedeToDos = localStorage.getItem(TODOS_LS)
     if(loadedeToDos !== null){
