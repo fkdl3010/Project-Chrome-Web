2a32951629ba9218c580b8b4f8428160a7cab1d1
fkdl3010 <fkdl3919@icloud.com>
2021-01-22 08:24:48 +0900

    [add api 2021-01-22]

--- weather.js
+++ weather.js
@@ -1,4 +1,12 @@
 const COORDS = 'coords';
+// openweathermap 에서 받아온 키
+const API_KEY = '6f79de1a3740b92afbe3573875b56559';
+
+// 요청 url 값
+// lat, lng 위도 경도
+function getWeather(lat, lon){
+    fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +20,7 @@ function handleGeoSucces(position){
         longitude // longitude==longitude
     }
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(){
@@ -29,6 +38,8 @@ function loadCoords(){
         askForCoords();
     }else{
         // getWeather
+        const parseCoords = JSON.parse(loadedCords);
+        console.log(loadedCords);
     }
 }
 
