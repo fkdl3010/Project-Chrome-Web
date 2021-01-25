6d9d7df278b83086e50fa3bc02d84c7fc9ada4a3
fkdl3010 <fkdl3919@icloud.com>
2021-01-25 09:02:15 +0900

    [add api ui 2021-01-25]

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,5 @@
+const weather = document.querySelector('.js-weather');
+
 const COORDS = 'coords';
 // openweathermap 에서 받아온 키
 const API_KEY = '6f79de1a3740b92afbe3573875b56559';
@@ -5,7 +7,18 @@ const API_KEY = '6f79de1a3740b92afbe3573875b56559';
 // 요청 url 값
 // lat, lng 위도 경도
 function getWeather(lat, lon){
-    fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    })
+        
 }
 
 function saveCoords(coordsObj){
@@ -39,7 +52,7 @@ function loadCoords(){
     }else{
         // getWeather
         const parseCoords = JSON.parse(loadedCords);
-        console.log(loadedCords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
