c5fb42c4654d05d35db29e236082bec08e2a3b83
fkdl3010 <fkdl3919@icloud.com>
2021-01-28 04:23:11 +0900

    image

--- js/bg.js
+++ js/bg.js
@@ -1,13 +1,20 @@
 const body = document.querySelector('body');
 
 const IMG_NUMBER = 7;
+const USER_LS_BG = "currentUser";
 
 
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add('bgImage');
+    const currentUser = localStorage.getItem(USER_LS_BG);
+    if(currentUser === null){
+        image.src = `images/1.jpg`;
+        image.classList.add('bgImage');
+    }else{
+        image.src = `images/${imgNumber + 1}.jpg`;
+        image.classList.add('bgImage');
+    }
     body.appendChild(image);
 }
 
